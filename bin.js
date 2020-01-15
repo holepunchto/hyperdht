c580d9a3d66b08a3a10ec8f48a5fc3d2927cabef
Bendik Aagaard Lynghaug <bendik.lynghaug@gmail.com>
2020-01-15 11:01:36 +0100

    Add quiet option to bin
    Nice for no-logging sd-card eating nodes

--- bin.js
+++ bin.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const dht = require('./')()
+const quiet = process.argv.includes('--quiet')
 
 console.log('node id: ' + dht.id.toString('hex'))
 
@@ -8,14 +9,16 @@ dht.on('ready', function () {
   console.log('dht node fully bootstrapped')
 })
 
-dht.on('announce', function (target, peer) {
-  console.log('received announce', target, peer)
-})
+if (!quiet) {
+  dht.on('announce', function (target, peer) {
+    console.log('received announce', target, peer)
+  })
 
-dht.on('unannounce', function (target, peer) {
-  console.log('received unannounce', target, peer)
-})
+  dht.on('unannounce', function (target, peer) {
+    console.log('received unannounce', target, peer)
+  })
 
-dht.on('lookup', function (target, peer) {
-  console.log('received lookup', target, peer)
-})
+  dht.on('lookup', function (target, peer) {
+    console.log('received lookup', target, peer)
+  })
+}
