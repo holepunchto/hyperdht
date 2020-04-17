a07017f668a0f32501b0f852f048239eb0a2dc00
Mathias Buus <mathiasbuus@gmail.com>
2020-04-17 23:20:28 +0200

    persist dht id and run eph+adaptive

--- bin.js
+++ bin.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
-const dht = require('./')()
-const quiet = process.argv.includes('--quiet')
+const os = require('os')
+const path = require('path')
+const fs = require('fs')
+const dhtrpc = require('dht-rpc')
+
+const ephemeral = !process.argv.includes('--non-ephemeral')
+const idFile = path.join(os.tmpdir(), 'dht-rpc-id')
+if (!fs.existsSync(idFile)) fs.writeFileSync(idFile, dhtrpc.id())
+const id = fs.readFileSync(idFile).slice(0, 32)
+const dht = require('./')({ ephemeral, adaptive: true, id })
 
 console.log('node id: ' + dht.id.toString('hex'))
+const quiet = process.argv.includes('--quiet')
 
 dht.on('ready', function () {
   console.log('dht node fully bootstrapped')
