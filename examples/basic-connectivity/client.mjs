3b6fc04fa65f727bd06590169ac8cc2859106996
Mathias Buus <mathiasbuus@gmail.com>
2021-09-23 13:52:34 +0200

    fix exmaple

--- examples/basic-connectivity/client.mjs
+++ examples/basic-connectivity/client.mjs
@@ -1,31 +1,32 @@
 import DHT from '../../index.js'
-import Client from '../../lib/client.js'
 
 const node = new DHT({
   quickFirewall: false,
   ephemeral: true // just setting this because this is a demo file
 })
 
-await node.ready()
-
-console.log('----------- ready -----------')
 printInfo()
 
 // Obvs no security implied here!
 const serverKeyPair = DHT.keyPair(Buffer.alloc(32).fill('basic-connectivity-server'))
-const clientKeyPair = DHT.keyPair(Buffer.alloc(32).fill('basic-connectivity-client'))
 
-const c = new Client(node, serverKeyPair.publicKey, clientKeyPair, {
+const encryptedSocket = node.connect(serverKeyPair.publicKey, {
   holepunch (remoteNat, localNat, remoteAddress, localAddr) {
     console.log('going to bail punch!', { remoteNat, localNat, remoteAddress, localAddr })
     return false
   }
 })
 
-const result = c.connect()
-
-result.on('open', function () {
+encryptedSocket.on('open', function () {
   console.log('Client connected!')
+})
+
+encryptedSocket.on('error', function (err) {
+  console.log('Client errored:', err)
+})
+
+encryptedSocket.on('close', function () {
+  console.log('Client closed...')
 })
 
 async function printInfo () {
