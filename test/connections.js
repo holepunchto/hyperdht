c395cf4afff16a7e06ca81efe732bccbd972b990
Mathias Buus <mathiasbuus@gmail.com>
2021-09-30 10:18:34 +0200

    more tests and fix minor issues

--- test/connections.js
+++ test/connections.js
@@ -1,8 +1,10 @@
 const test = require('brittle')
 const { swarm } = require('./helpers')
+const { once } = require('events')
+const DHT = require('../')
 
-test('createServer + connect once', async function (t) {
-  const [a, b] = await swarm(t, 32)
+test('createServer + connect - once defaults', async function (t) {
+  const [a, b] = await swarm(t)
   const lc = t.test('socket lifecycle')
 
   t.plan(2)
@@ -38,4 +40,131 @@ test('createServer + connect once', async function (t) {
   })
 
   await server.close()
+})
+
+test('createServer + connect - force holepunch', async function (t) {
+  const [boot] = await swarm(t)
+
+  const bootstrap = [{ host: '127.0.0.1', port: boot.address().port }]
+  const a = new DHT({ bootstrap, quickFirewall: false, ephemeral: true })
+  const b = new DHT({ bootstrap, quickFirewall: false, ephemeral: true })
+
+  await a.ready()
+  await b.ready()
+
+  const lc = t.test('socket lifecycle')
+  lc.plan(6)
+
+  const server = a.createServer(function (socket) {
+    lc.ok(!!socket.rawStream._utp, 'server is utp') // TODO: make this easier to detect!
+    lc.pass('utp server side opened')
+
+    socket.once('end', function () {
+      lc.pass('utp server side ended')
+      socket.end()
+    })
+  })
+
+  await server.listen()
+
+  const socket = b.connect(server.publicKey)
+
+  socket.once('open', function () {
+    lc.ok(!!socket.rawStream._utp, 'client is utp') // TODO: make this easier to detect!
+    lc.pass('utp client side opened')
+  })
+
+  socket.once('end', function () {
+    lc.pass('utp client side ended')
+  })
+
+  socket.end()
+
+  await lc
+
+  await server.close()
+  await a.destroy()
+  await b.destroy()
+})
+
+test('server choosing to abort holepunch', async function (t) {
+  const [boot] = await swarm(t)
+
+  const bootstrap = [{ host: '127.0.0.1', port: boot.address().port }]
+  const a = new DHT({ bootstrap, quickFirewall: false, ephemeral: true })
+  const b = new DHT({ bootstrap, quickFirewall: false, ephemeral: true })
+
+  await a.ready()
+  await b.ready()
+
+  const lc = t.test('socket lifecycle')
+  lc.plan(2)
+
+  const server = a.createServer({
+    holepunch () {
+      lc.pass('server should trigger holepuncher hook')
+      return false
+    }
+  }, function (socket) {
+    lc.fail('server should not make a connection')
+  })
+
+  await server.listen()
+
+  const socket = b.connect(server.publicKey)
+
+  socket.once('open', function () {
+    lc.fail('client should not make a connection')
+  })
+
+  socket.once('error', function (err) {
+    lc.ok(!!err, 'client socket should error')
+  })
+
+  await lc
+
+  await server.close()
+  await a.destroy()
+  await b.destroy()
+})
+
+test('client choosing to abort holepunch', async function (t) {
+  const [boot] = await swarm(t)
+
+  const bootstrap = [{ host: '127.0.0.1', port: boot.address().port }]
+  const a = new DHT({ bootstrap, quickFirewall: false, ephemeral: true })
+  const b = new DHT({ bootstrap, quickFirewall: false, ephemeral: true })
+
+  await a.ready()
+  await b.ready()
+
+  const lc = t.test('socket lifecycle')
+  lc.plan(2)
+
+  const server = a.createServer(function (socket) {
+    lc.fail('server should not make a connection')
+  })
+
+  await server.listen()
+
+  const socket = b.connect(server.publicKey, {
+    holepunch () {
+      lc.pass('client is aborting')
+      return false
+    }
+  })
+
+  socket.once('open', function () {
+    lc.fail('client should not make a connection')
+  })
+
+  socket.once('error', function (err) {
+    lc.ok(!!err, 'client socket should error')
+  })
+
+  await lc
+
+  await server.close()
+  await a.destroy()
+  await b.destroy()
 })
