9d46767e573b3e0f80cb4e9602df4a357aa964f2
Mathias Buus <mathiasbuus@gmail.com>
2021-11-06 14:45:47 +0100

    add test for connect event and force bump deps

--- test/connections.js
+++ test/connections.js
@@ -42,6 +42,45 @@ test('createServer + connect - once defaults', async function (t) {
   await server.close()
 })
 
+test('createServer + connect - emits connect', async function (t) {
+  const [a, b] = await swarm(t)
+  const lc = t.test('socket lifecycle')
+
+  t.plan(2)
+  lc.plan(4)
+
+  const server = a.createServer(function (socket) {
+    lc.pass('server side opened')
+
+    socket.once('end', function () {
+      lc.pass('server side ended')
+      socket.end()
+    })
+  })
+
+  await server.listen()
+
+  const socket = b.connect(server.publicKey)
+
+  socket.once('connect', function () {
+    lc.pass('client side emitted connect')
+  })
+
+  socket.once('end', function () {
+    lc.pass('client side ended')
+  })
+
+  socket.end()
+
+  await lc
+
+  server.on('close', function () {
+    t.pass('server closed')
+  })
+
+  await server.close()
+})
+
 test('createServer + connect - exchange data', async function (t) {
   const [a, b] = await swarm(t)
   const lc = t.test('socket lifecycle')
