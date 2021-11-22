19dc573b88e50cbacc833bb4844fb9d0f05522ac
Mathias Buus <mathiasbuus@gmail.com>
2021-11-22 15:33:36 +0100

    fix typo in mutable put, fixes #63

--- test/storing.js
+++ test/storing.js
@@ -40,3 +40,25 @@ test('mutable put - get', async function (t) {
   t.is(typeof res.to.host, 'string')
   t.is(typeof res.to.port, 'number')
 })
+
+test('mutable put - put - get', async function (t) {
+  const nodes = await swarm(t, 100)
+  const keyPair = HyperDHT.keyPair()
+
+  const put = await nodes[30].mutablePut(keyPair, Buffer.from('testing'))
+
+  t.is(put.signature.length, 64)
+  t.is(put.seq, 0)
+
+  const put2 = await nodes[25].mutablePut(keyPair, Buffer.from('testing two'), { seq: 2 })
+
+  t.is(put2.signature.length, 64)
+  t.is(put2.seq, 2)
+
+  const res = await nodes[3].mutableGet(keyPair.publicKey)
+
+  t.is(res.seq, 2)
+  t.is(Buffer.isBuffer(res.value), true)
+  t.is(Buffer.compare(res.signature, put2.signature), 0)
+  t.is(res.value.toString(), 'testing two')
+})
