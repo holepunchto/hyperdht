84146b40736545c32522469d7a10e640365f8690
Mathias Buus <mathiasbuus@gmail.com>
2022-02-01 12:00:24 +0100

    add failing halfopen test

--- test/connections.js
+++ test/connections.js
@@ -275,3 +275,27 @@ test('tcp noise, client ends, no crash', async function (t) {
   await new Promise((resolve) => sock.on('close', resolve))
   t.pass('did not crash')
 })
+
+test('half open', async function (t) {
+  t.plan(2)
+
+  const [, a] = await swarm(t)
+
+  const server = a.createServer()
+  await server.listen()
+
+  const socket = a.connect(server.address().publicKey)
+
+  server.on('connection', (socket) => {
+    socket.on('data', (data) => {
+      t.alike(data.toString(), 'ping')
+      setTimeout(() => socket.end('pong'))
+    })
+  })
+
+  socket
+    .on('data', (data) => {
+      t.alike(data.toString(), 'pong')
+    })
+    .end('ping')
+})
