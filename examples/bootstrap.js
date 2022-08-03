7e8ddf326f6d969f3695c88b843e6b9bf72301b6
Lucas Barrena <lucasbarrena98@gmail.com>
2022-08-02 22:03:00 -0300

    Update bootstrap.js

--- examples/bootstrap.js
+++ examples/bootstrap.js
@@ -1,6 +1,7 @@
 const DHT = require('../')
 
-const node = DHT.bootstrapper(49737)
+// You must use the public IP of the server where this bootstrap is running
+const node = DHT.bootstrapper(49737, '127.0.0.1')
 
 node.ready().then(function () {
   console.log('Bootstrapper running on port ' + node.address().port)
