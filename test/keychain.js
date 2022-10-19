ee66e64c4c9348ba22fba4f4f6085259d6e7f87e
Lucas Barrena <lucasbarrena98@gmail.com>
2022-10-19 13:07:48 -0300

    better tests

--- test/keychain.js
+++ test/keychain.js
@@ -2,44 +2,74 @@ const test = require('brittle')
 const { swarm } = require('./helpers')
 const Keychain = require('keypear')
 
-test('createServer with keypear + connect', async function (t) {
-  t.plan(2)
-
-  const keys = new Keychain()
+test('server with keychain', async function (t) {
+  t.plan(4)
 
   const [a, b] = await swarm(t)
-  const lc = t.test('socket lifecycle')
+  const keys = new Keychain()
+  const serverKeyPair = keys.get()
 
-  lc.plan(4)
-
-  const server = a.createServer(function (socket) {
-    lc.pass('server side opened')
-
-    socket.once('end', function () {
-      lc.pass('server side ended')
-      socket.end()
-    })
-  })
-
-  await server.listen(keys.get())
+  const server = a.createServer()
+  await server.listen(serverKeyPair)
+  t.is(server.publicKey, serverKeyPair.publicKey)
 
   const socket = b.connect(server.publicKey)
 
-  socket.once('open', function () {
-    lc.pass('client side opened')
+  server.on('connection', (socket) => {
+    t.is(socket.publicKey, server.publicKey)
+    t.pass('server connected')
+    socket.end()
   })
 
-  socket.once('end', function () {
-    lc.pass('client side ended')
+  socket.on('open', () => {
+    t.pass('client connected')
+    socket.end()
+  })
+})
+
+test('client with keychain', async function (t) {
+  t.plan(4)
+
+  const [a, b] = await swarm(t)
+  const keys = new Keychain()
+  const clientKeyPair = keys.get()
+
+  const server = a.createServer()
+  await server.listen()
+
+  const socket = b.connect(server.publicKey, { keyPair: clientKeyPair })
+  t.is(socket.publicKey, clientKeyPair.publicKey)
+
+  server.on('connection', (socket) => {
+    t.alike(socket.remotePublicKey, clientKeyPair.publicKey)
+    t.pass('server connected')
+    socket.end()
   })
 
-  socket.end()
+  socket.on('open', () => {
+    t.pass('client connected')
+    socket.end()
+  })
+})
 
-  await lc
+test('server and client, both with keychain', async function (t) {
+  t.plan(2)
 
-  server.on('close', function () {
-    t.pass('server closed')
+  const [a, b] = await swarm(t)
+  const keys = new Keychain()
+
+  const server = a.createServer()
+  await server.listen(keys.get('server'))
+
+  const socket = b.connect(server.publicKey, { keyPair: keys.get('client') })
+
+  server.on('connection', (socket) => {
+    t.pass('server connected')
+    socket.end()
   })
 
-  await server.close()
+  socket.on('open', () => {
+    t.pass('client connected')
+    socket.end()
+  })
 })
