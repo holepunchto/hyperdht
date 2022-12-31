c78d9a3616ffb4a923a6f2685a7891d9b73c92a5
Lucas Barrena <lucasbarrena98@gmail.com>
2022-12-31 02:34:23 -0300

    Update bootstrapper.js

--- test/bootstrapper.js
+++ test/bootstrapper.js
@@ -3,10 +3,12 @@ const { toArray } = require('./helpers')
 const DHT = require('../')
 
 test('single node network is enough to find peers', async function (t) {
-  const bootstrap1 = DHT.bootstrapper(49737, '127.0.0.1', { host: '0.0.0.0' }) // + otherwise host binds to :: (IPv6), but it requires '127.0.0.1' (IPv4) as a bootstrap host
+  // const bootstrap1 = DHT.bootstrapper(49737, '127.0.0.1', { host: '0.0.0.0' }) // + otherwise host binds to :: (IPv6), but it requires '127.0.0.1' (IPv4) as a bootstrap host
+  const bootstrap1 = DHT.bootstrapper(49737, '127.0.0.1')
   await bootstrap1.ready()
 
-  const bootstrap = [bootstrap1.address()] // + a bootstrap like { host: '::', port: 49737 } doesn't work, but this one does: { host: '0.0.0.0', port: 49737 }
+  // const bootstrap = [bootstrap1.address()] // + a bootstrap like { host: '::', port: 49737 } doesn't work, but this one does: { host: '0.0.0.0', port: 49737 }
+  const bootstrap = [{ host: '127.0.0.1', port: bootstrap1.address().port }]
 
   const a = new DHT({ bootstrap, ephemeral: true })
   const server = a.createServer()
