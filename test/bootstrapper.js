aa9d3a5db583f6f8d09993542ab24311b05927e7
Lucas Barrena <lucasbarrena98@gmail.com>
2023-01-02 05:13:33 -0300

    add localIP to tests

--- test/bootstrapper.js
+++ test/bootstrapper.js
@@ -1,10 +1,13 @@
 const test = require('brittle')
 const { toArray } = require('./helpers')
 const DHT = require('../')
+const UDX = require('udx-native')
 
 // maybe '0.0.0.0' is just an invalid address for a dht node
 // because that just "listens" in all interfaces, so a node has effectively multiple connectable host addresses
 // but this dht node can only decide one valid address for the nat of itself, and i.e. other eph nodes will only be able to succesfully talk with that specific nat address
+
+console.log('localIP:', localIP())
 
 test('single node network is enough to find peers', async function (t) {
   const bootstrap1 = DHT.bootstrapper(49737, '127.0.0.1')
@@ -21,7 +24,7 @@ test('bootstrapper at localhost but bind to all networks (IPv6)', async function
   // t.ok(await makeServerAndClient([{ host: '0.0.0.0', port: 49737 }])) // + why this works? I think it should not work (and btw '::' doesn't)
 
   // This is fine: it doesn't work because of the added NAT that says '127.0.0.1' (as defined on bootstrapper arg), it's even while binding to '0.0.0.0' or '::' as it is
-  t.absent(await makeServerAndClient([{ host: '192.168.0.23', port: 49737 }])) // + local host address
+  t.absent(await makeServerAndClient([{ host: localIP(), port: 49737 }]))
 
   await bootstrap1.destroy()
 })
@@ -34,7 +37,7 @@ test('bootstrapper at localhost but bind to all networks (IPv4)', async function
   t.ok(await makeServerAndClient([{ host: '127.0.0.1', port: 49737 }]))
   // t.ok(await makeServerAndClient([{ host: '0.0.0.0', port: 49737 }])) // + maybe here makes sense but still why it works exactly?
 
-  t.absent(await makeServerAndClient([{ host: '192.168.0.23', port: 49737 }])) // + local host address
+  t.absent(await makeServerAndClient([{ host: localIP(), port: 49737 }])) // + local host address
 
   await bootstrap1.destroy()
 })
@@ -56,7 +59,7 @@ test('first persistent node with no host given', async function (t) {
   await bootstrap1.ready()
   t.is(bootstrap1.address().host, '::') // It still binds to all networks
 
-  t.ok(await makeServerAndClient([{ host: '192.168.0.23', port: bootstrap1.address().port }]))
+  t.ok(await makeServerAndClient([{ host: localIP(), port: bootstrap1.address().port }]))
 
   // This is fine: it doesn't work due different NAT host. I mean it's reachable but NAT host address doesn't match
   t.absent(await makeServerAndClient([{ host: '127.0.0.1', port: bootstrap1.address().port }]))
@@ -87,11 +90,11 @@ test('first persistent node with no host given but binds to localhost', async fu
 
 test('first persistent node with no host given but binds to local host address', async function (t) {
   // Host was given so it uses that one as a NAT host
-  const bootstrap1 = new DHT({ bootstrap: [], ephemeral: false, firewalled: false, host: '192.168.0.23' })
+  const bootstrap1 = new DHT({ bootstrap: [], ephemeral: false, firewalled: false, host: localIP() })
   await bootstrap1.ready()
-  t.is(bootstrap1.address().host, '192.168.0.23')
+  t.is(bootstrap1.address().host, localIP())
 
-  t.ok(await makeServerAndClient([{ host: '192.168.0.23', port: bootstrap1.address().port }]))
+  t.ok(await makeServerAndClient([{ host: localIP(), port: bootstrap1.address().port }]))
 
   await bootstrap1.destroy()
 })
@@ -110,4 +113,16 @@ async function makeServerAndClient (bootstrap) {
   await a.destroy()
 
   return success
+}
+
+// i.e. 192.168.0.23
+function localIP () {
+  const udx = new UDX()
+  let host = null
+  for (const n of udx.networkInterfaces()) {
+    if (n.family !== 4 || n.internal) continue
+    if (n.name === 'en0') return n.host
+    if (host === null) host = n.host
+  }
+  return host || '127.0.0.1'
 }
