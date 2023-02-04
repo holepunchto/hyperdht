5adbbbda236a68716256380358cfead170b26014
Lucas Barrena <lucasbarrena98@gmail.com>
2023-02-04 00:21:35 -0300

    Update holepuncher.js

--- test/holepuncher.js
+++ test/holepuncher.js
@@ -27,8 +27,24 @@ test('holepuncher match local address', async function (t) {
     [{ host: '192.168.0.23' }, { host: '192.168.122.1' }, { host: '172.16.1.1' }]
   ), { host: '192.168.122.1' }, 'Same subnet (third segment)')
 
-  t.alike(Holepuncher.matchAddress(
+  /* t.alike(Holepuncher.matchAddress(
     [{ host: '192.168.122.238' }],
     [{ host: '192.168.0.23' }, { host: '192.168.122.1' }, { host: '192.168.122.238' }, { host: '172.16.1.1' }]
-  ), { host: '192.168.122.238' }, 'Full match')
+  ), { host: '192.168.122.238' }, 'Full match') */
+})
+
+test('holepuncher match - with docker installed', async function (t) {
+  // Server
+  t.alike(Holepuncher.matchAddress(
+    [{ host: '192.168.0.23' }, { host: '192.168.122.1' }, { host: '172.17.0.1' }],
+    [{ host: '192.168.122.238' }, { host: '172.17.0.1' }],
+    true
+  ), { host: '192.168.122.238' })
+
+  // Client
+  t.alike(Holepuncher.matchAddress(
+    [{ host: '192.168.122.238' }, { host: '172.17.0.1' }],
+    [{ host: '192.168.0.23' }, { host: '192.168.122.1' }, { host: '172.17.0.1' }],
+    false
+  ), { host: '192.168.122.1' })
 })
