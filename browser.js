595251bea9633d7e3648e2a0177f46c071efac4c
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-03-16 09:30:19 +0100

    Rename to `hyperdht`

--- browser.js
+++ browser.js
@@ -2,7 +2,7 @@ const { hash, createKeyPair } = require('./lib/crypto')
 
 module.exports = class Stub {
   constructor () {
-    throw new Error('@hyperswarm/dht is not supported in browsers')
+    throw new Error('hyperdht is not supported in browsers')
   }
 
   static keyPair (seed) {
