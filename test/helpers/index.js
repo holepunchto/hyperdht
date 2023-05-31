97440fb2c5eed2f332f0e97c54f7e5bbaf4a8b22
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-05-31 09:54:06 +0200

    Add `hyperdht/testnet` module

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,6 +1,6 @@
-const HyperDHT = require('../../')
+const createTestnet = require('../../testnet')
 
-module.exports = { swarm, destroy, toArray }
+module.exports = { swarm, toArray }
 
 async function toArray (iterable) {
   const result = []
@@ -8,38 +8,6 @@ async function toArray (iterable) {
   return result
 }
 
-async function destroy (...nodes) {
-  for (const node of nodes) {
-    for (const server of node.listening) await server.close()
-  }
-
-  for (let i = nodes.length - 1; i >= 0; i--) {
-    const node = nodes[i]
-
-    if (Array.isArray(node)) await destroy(...node)
-    else await node.destroy()
-  }
-}
-
 async function swarm (t, n = 32, bootstrap = []) {
-  const nodes = []
-  while (nodes.length < n) {
-    const node = new HyperDHT({ bootstrap, ephemeral: false })
-    await node.ready()
-    if (!bootstrap.length) bootstrap = [{ host: '127.0.0.1', port: node.address().port }]
-    nodes.push(node)
-  }
-  t.teardown(() => destroy(...nodes))
-  return {
-    nodes,
-    bootstrap,
-    createNode (opts = {}) {
-      const node = new HyperDHT({ bootstrap, ephemeral: true, ...opts })
-      nodes.push(node)
-      return node
-    },
-    [Symbol.iterator] () {
-      return nodes[Symbol.iterator]()
-    }
-  }
+  return createTestnet(n, { bootstrap, teardown: t.teardown })
 }
