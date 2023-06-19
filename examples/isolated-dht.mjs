7ae4e970a7367114c39b39d580aced1efadd61fb
Lucas Barrena <lucasbarrena98@gmail.com>
2023-06-19 15:20:03 -0300

    Align things

--- examples/isolated-dht.mjs
+++ examples/isolated-dht.mjs
@@ -1,27 +1,29 @@
 import DHT from '../index.js'
 
-// [PART 1]
-// Let's say I used DigitalOcean to create a server and it has a public IP:
-const MY_PUBLIC_IP = '67.205.140.151' // If you're on a local machine then use '127.0.0.1' or the corresponding internal address
+// [BOOTSTRAP NODE]
+// Let's say you created a VPS and it has a public IP, you would use it here:
+const MY_PUBLIC_IP = '127.0.0.1' // If it's for a local network then use the corresponding local address like 192.168.0.15
 
 // Inside that server, to create our first bootstrap node:
 const bootstrap1 = DHT.bootstrapper(49737, MY_PUBLIC_IP)
 await bootstrap1.ready()
 // Your bootstrap node is now running at 67.205.140.151:49737 and it's globally accessible for anyone to use it.
 
+// CLI equivalent for creating the first bootstrap node:
+// hyperdht --bootstrap --host 127.0.0.1 --port 49737
+
 // This would be the list of bootstrap nodes:
 const bootstrap = [{ host: MY_PUBLIC_IP, port: bootstrap1.address().port }]
-// You can achieve redundancy by having 2-3 servers with the same setup (PART 1 + PART 2) in each one.
+// You can achieve redundancy by having 2-3 servers with the same setup above in each server.
 
 // [THREE IMPORTANT CONCEPTS]
-// We would like to use our new bootstrap to create nodes but before that:
 // By default, all nodes joining a DHT network are considered ephemeral nodes.
 
 // What is "ephemeral"?
 // 1) Low uptime (less than ~30 mins online).
 // 2) It may or not be firewalled.
 
-// Nodes adapt themselves by itselfs. After a good time they can automatically become persistent nodes.
+// Nodes adapt themselves. After a good time they can automatically become persistent nodes.
 
 // What is "persistent"?
 // 1) High uptime (at least ~30 mins online).
@@ -32,25 +34,10 @@ const bootstrap = [{ host: MY_PUBLIC_IP, port: bootstrap1.address().port }]
 // What is a "bootstrap node"?
 // 1) You know the ip:port (i.e. static IP or DNS).
 // 2) It's not firewalled.
-// 3) It's always running, as it has to be online for new nodes to join.
+// 3) It's always running, it has to be online for new nodes to join. Unless peers know any other persistent nodes.
 
-// [PART 2]
-// Besides the bootstrap node, to have a complete DHT network: we need one persistent node.
-
-// You could have nodes that organically become persistent, but that takes ~30 mins.
-// Our network is new and no one is using it, so we need to create a forced persistent node to avoid waiting.
-
-// Inside the same server, create a new persistent node:
-const persistent1 = new DHT({ bootstrap, ephemeral: false })
-await persistent1.ready()
-// CAUTION:
-// As we're the ones creating the network, we provide at least one persistent node, this is for the network to be fully operational.
-// From now on, nobody uses the {ephemeral:false} option to create nodes, otherwise you could damage the network health.
-// If you or other people wants to provide more persistent nodes then everyone must wait to organically become persistent.
-
-// [PART 3]
+// [USAGE]
 // Now is time to use the network as a normal user!
-
 const node1 = new DHT({ bootstrap })
 const node2 = new DHT({ bootstrap })
 
--- test/bootstrapper.js
+++ test/bootstrapper.js
@@ -1,48 +1,47 @@
 const test = require('brittle')
-const { toArray } = require('./helpers')
+const { toArray, freePort } = require('./helpers')
 const DHT = require('../')
-const UDX = require('udx-native')
 
 test('single node network is enough to find peers', async function (t) {
-  const bootstrap1 = DHT.bootstrapper(49737, '127.0.0.1')
-  t.ok(await makeServerAndClient([{ host: '127.0.0.1', port: 49737 }]))
+  const port = await freePort()
+
+  const bootstrap1 = DHT.bootstrapper(port, '127.0.0.1')
+  t.ok(await makeServerAndClient([{ host: '127.0.0.1', port }]))
   await bootstrap1.destroy()
 })
 
 test('bootstrapper at localhost but bind to all networks (IPv6)', async function (t) {
-  const bootstrap1 = DHT.bootstrapper(49737, '127.0.0.1') // NAT host / peer.id is given
+  const port = await freePort()
+
+  const bootstrap1 = DHT.bootstrapper(port, '127.0.0.1') // NAT host / peer.id is given
   await bootstrap1.ready()
-  t.is(bootstrap1.address().host, '0.0.0.0') // It still binds to all networks (if this is a problem, is kind of improvable)
+  t.is(bootstrap1.address().host, '0.0.0.0') // It still binds to all networks
 
-  t.ok(await makeServerAndClient([{ host: '127.0.0.1', port: 49737 }]))
-  // t.ok(await makeServerAndClient([{ host: '0.0.0.0', port: 49737 }])) // + why this works? I think it should not work (and btw '::' doesn't)
-
-  // It's reachable but NAT host address, peer id or something doesn't match (as defined on bootstrapper arg)
-  t.absent(await makeServerAndClient([{ host: localIP(), port: 49737 }]))
+  t.ok(await makeServerAndClient([{ host: '127.0.0.1', port }]))
 
   await bootstrap1.destroy()
 })
 
 test('bootstrapper at localhost but bind to all networks (IPv4)', async function (t) {
-  const bootstrap1 = DHT.bootstrapper(49737, '127.0.0.1', { host: '0.0.0.0' })
+  const port = await freePort()
+
+  const bootstrap1 = DHT.bootstrapper(port, '127.0.0.1', { host: '0.0.0.0' })
   await bootstrap1.ready()
   t.is(bootstrap1.address().host, '0.0.0.0')
 
-  t.ok(await makeServerAndClient([{ host: '127.0.0.1', port: 49737 }]))
-  // t.ok(await makeServerAndClient([{ host: '0.0.0.0', port: 49737 }])) // + maybe here makes sense but still why it works exactly?
-
-  t.absent(await makeServerAndClient([{ host: localIP(), port: 49737 }]))
+  t.ok(await makeServerAndClient([{ host: '127.0.0.1', port }]))
 
   await bootstrap1.destroy()
 })
 
 test('bootstrapper at localhost and also bind to localhost', async function (t) {
-  const bootstrap1 = DHT.bootstrapper(49737, '127.0.0.1', { host: '127.0.0.1' })
+  const port = await freePort()
+
+  const bootstrap1 = DHT.bootstrapper(port, '127.0.0.1', { host: '127.0.0.1' })
   await bootstrap1.ready()
   t.is(bootstrap1.address().host, '127.0.0.1')
 
-  t.ok(await makeServerAndClient([{ host: '127.0.0.1', port: 49737 }]))
-  // t.ok(await makeServerAndClient([{ host: '0.0.0.0', port: 49737 }])) // + why this still works?
+  t.ok(await makeServerAndClient([{ host: '127.0.0.1', port }]))
 
   await bootstrap1.destroy()
 })
@@ -63,14 +62,4 @@ async function makeServerAndClient (bootstrap) {
   await a.destroy()
 
   return success
-}
-
-// i.e. 192.168.0.23
-function localIP (family = 4) {
-  const udx = new UDX()
-  for (const n of udx.networkInterfaces()) {
-    if (n.family !== family || n.internal) continue
-    return n.host
-  }
-  return family === 4 ? '127.0.0.1' : '::1'
 }
