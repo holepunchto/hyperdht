cd99d7bbfdd5b29069b0af591408731ca0cb6e65
Tobias Baunbaek <freeall@gmail.com>
2024-03-20 14:19:13 +0100

    Restarting client forever

--- test/integration/fixtures/server-through-relay.js
+++ test/integration/fixtures/server-through-relay.js
@@ -3,28 +3,24 @@ const DHT = require('../../')
 const publicKey = Buffer.from(process.argv[2], 'hex')
 const secretKey = Buffer.from(process.argv[3], 'hex')
 const relayServerPublicKey = Buffer.from(process.argv[4], 'hex')
+const bootstrap = JSON.parse(process.argv[5])
 const keyPair = { publicKey, secretKey }
+
 main()
 
-console.log('publicKey', publicKey.toString('hex'))
-console.log('secretKey', secretKey.toString('hex'))
-console.log('relayServerPublicKey', relayServerPublicKey.toString('hex'))
-
 async function main () {
-  const node = new DHT()
+  const node = new DHT({ bootstrap })
   const server = node.createServer({
     holepunch: false, // To ensure it relies only on relaying
     shareLocalAddress: false, // To help ensure it relies only on relaying (otherwise it can connect directly over LAN, without even trying to holepunch)
     relayThrough: relayServerPublicKey
   }, socket => {
+    socket.setKeepAlive(5000)
     socket
-      .on('open', () => console.log('socket_opened'))
-      .on('data', data => {
-        console.log(`socket_ondata ${data.toString('hex')}`)
-        // client.destroy()
-      })
+      .on('open', () => console.log('socket_onopen'))
       .on('close', () => console.log('socket_onclose'))
       .on('error', err => console.log(`socket_onerror ${err.code}`))
+    setTimeout(() => socket.destroy(), 1000 + 10 * Math.random())
   })
   server.on('open', () => console.log('server_onopen'))
   server.on('error', err => console.log(`server_onerror ${err.code}`))
