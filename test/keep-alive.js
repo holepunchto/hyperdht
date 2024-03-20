70093ac6a8517f143167c1ab5e1102bb400fd2c7
Tobias Baunbaek <freeall@gmail.com>
2024-03-20 08:35:10 +0100

    Use path

--- test/keep-alive.js
+++ test/keep-alive.js
@@ -1,6 +1,7 @@
 const test = require('brittle')
 const DHT = require('../')
 const { spawn } = require('child_process')
+const path = require('path')
 
 // Server is run in a separate proces to make sure that the client doesn't see
 // the socket is closed from being run by the same process
@@ -11,7 +12,7 @@ test('Client use keepalive to detect disconnect - separated by processes', async
   const publicKey = keyPair.publicKey.toString('hex')
   const secretKey = keyPair.secretKey.toString('hex')
 
-  const serverProcess = spawn('node', ['test/fixtures/server.js', publicKey, secretKey])
+  const serverProcess = spawn('node', [path.join(__dirname, 'fixtures/server.js'), publicKey, secretKey])
   serverProcess.stderr.on('data', () => t.fail())
   serverProcess.stdout.on('data', data => {
     data = data.toString().trim()
@@ -47,7 +48,7 @@ test('Client not using keepalive does not detect disconnect - separated by proce
   const publicKey = keyPair.publicKey.toString('hex')
   const secretKey = keyPair.secretKey.toString('hex')
 
-  const serverProcess = spawn('node', ['test/fixtures/server.js', publicKey, secretKey])
+  const serverProcess = spawn('node', [path.join(__dirname, 'fixtures/server.js'), publicKey, secretKey])
   serverProcess.stderr.on('data', () => t.fail())
   serverProcess.stdout.on('data', data => {
     data = data.toString().trim()
