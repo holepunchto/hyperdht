83079df186a9b8fec374ef062c4cc05488d32123
Tobias Baunbaek <freeall@gmail.com>
2024-03-21 11:22:37 +0100

    Moved spawnFixture to helper. Added to both keep-alive tests

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,6 +1,8 @@
 const createTestnet = require('../../testnet')
+const NewlineDecoder = require('newline-decoder')
+const { spawn } = require('child_process')
 
-module.exports = { swarm, toArray }
+module.exports = { swarm, toArray, spawnFixture }
 
 async function toArray (iterable) {
   const result = []
@@ -10,4 +12,16 @@ async function toArray (iterable) {
 
 async function swarm (t, n = 32, bootstrap = []) {
   return createTestnet(n, { bootstrap, teardown: t.teardown })
+}
+
+async function * spawnFixture (t, args) {
+  const proc = spawn(process.execPath, args)
+  const nl = new NewlineDecoder()
+
+  proc.stderr.on('data', err => t.fail(err))
+  const kill = () => setTimeout(() => proc.kill('SIGKILL'), 1000)
+
+  for await (const data of proc.stdout) {
+    for (const line of nl.push(data)) yield [kill, line]
+  }
 }
--- test/integration/keep-alive.js
+++ test/integration/keep-alive.js
@@ -1,45 +1,37 @@
 const test = require('brittle')
 const DHT = require('../../')
-const { spawn } = require('child_process')
 const path = require('path')
-const NewlineDecoder = require('newline-decoder')
-const { swarm } = require('../helpers')
+const { swarm, spawnFixture } = require('../helpers')
 
 // Server is run in a separate proces to make sure that we can forcefully close it.
 // If the server called `socket.destroy()` that would send an unacked packet back
 // to the client and it would notice that the socket has been closed - and thus the
 // keepalive timeout would never kick in.
 
-async function * spawnFixture (t, args) {
-  const proc = spawn(process.execPath, args)
-  const nl = new NewlineDecoder()
-
-  proc.stderr.on('data', err => t.fail(err))
-  const kill = () => setTimeout(() => proc.kill('SIGKILL'), 1000)
-
-  for await (const data of proc.stdout) {
-    for (const line of nl.push(data)) yield [kill, line]
-  }
-
-  t.pass('died')
-}
-
 test('Client use keepalive to detect disconnect - separated by processes', async t => {
   t.plan(2)
   t.teardown(() => node.destroy())
 
   const clientTest = t.test('client')
-
-  clientTest.plan(3)
-
   const { bootstrap } = await swarm(t)
   const node = new DHT({ bootstrap })
   const keyPair = DHT.keyPair()
   const publicKey = keyPair.publicKey.toString('hex')
   const secretKey = keyPair.secretKey.toString('hex')
 
-  t.test('server', async function (serverTest) {
-    for await (const [kill, data] of spawnFixture(serverTest, [path.join(__dirname, 'fixtures/server.js'), publicKey, secretKey, JSON.stringify(bootstrap)])) {
+  clientTest.plan(3)
+
+  t.test('server', async serverTest => {
+    serverTest.plan(3)
+
+    const args = [
+      path.join(__dirname, 'fixtures/server.js'),
+      publicKey,
+      secretKey,
+      JSON.stringify(bootstrap)
+    ]
+
+    for await (const [kill, data] of spawnFixture(serverTest, args)) {
       if (data === 'started') {
         serverTest.pass('Started. Now starting client')
         startClient()
@@ -50,6 +42,8 @@ test('Client use keepalive to detect disconnect - separated by processes', async
         kill()
       }
     }
+
+    serverTest.pass('Process died')
   })
 
   function startClient () {
@@ -62,47 +56,54 @@ test('Client use keepalive to detect disconnect - separated by processes', async
 })
 
 test('Client not using keepalive does not detect disconnect - separated by processes', async t => {
-  t.plan(4)
+  t.plan(2)
   t.teardown(() => node.destroy())
 
+  const clientTest = t.test('client')
   const { bootstrap } = await swarm(t)
   const node = new DHT({ bootstrap })
   const keyPair = DHT.keyPair()
   const publicKey = keyPair.publicKey.toString('hex')
   const secretKey = keyPair.secretKey.toString('hex')
   let timedout = false
-  const serverProcess = spawn(process.execPath, [
-    path.join(__dirname, 'fixtures/server.js'),
-    publicKey,
-    secretKey,
-    JSON.stringify(bootstrap)
-  ])
 
-  serverProcess.stderr.on('data', () => t.fail())
-  serverProcess.stdout.on('data', data => {
-    data = data.toString().trim()
-    const isStarted = data === 'started'
-    const isSocketConnected = data === 'socket_connected'
-    if (isStarted) {
-      t.pass('[server] Started. Now starting client')
-      startClient()
-    }
-    if (isSocketConnected) {
-      t.pass('[server] Client connected. Killing server process in 1 second')
-      setTimeout(() => serverProcess.kill('SIGKILL'), 1000) // Wait a bit to make sure the handshake has happened
-      setTimeout(() => {
-        timedout = true
+  clientTest.plan(2)
 
-        t.pass('After 20 seconds the connection is still open')
-      }, 20000)
+  t.test('server', async serverTest => {
+    serverTest.plan(3)
+
+    const args = [
+      path.join(__dirname, 'fixtures/server.js'),
+      publicKey,
+      secretKey,
+      JSON.stringify(bootstrap)
+    ]
+
+    for await (const [kill, data] of spawnFixture(serverTest, args)) {
+      if (data === 'started') {
+        serverTest.pass('Started. Now starting client')
+        startClient()
+      }
+
+      if (data === 'socket_connected') {
+        serverTest.pass('Client connected. Killing server process in 1 second')
+        kill()
+      }
     }
+
+    serverTest.pass('Process died')
+
+    setTimeout(() => {
+      timedout = true
+      clientTest.pass('After 20 seconds the connection is still open')
+    }, 20000)
   })
 
   function startClient () {
     const clientSocket = node.connect(publicKey)
     // No .setKeepAlive() here
-    clientSocket.on('open', () => t.pass('[client] Connected'))
-    clientSocket.on('error', () => !timedout && t.fail())
-    clientSocket.on('close', () => !timedout && t.fail())
+    clientSocket.on('open', () => clientTest.pass('Connected'))
+    clientSocket.on('error', () => !timedout && clientTest.fail())
+    clientSocket.on('close', () => !timedout && clientTest.fail())
   }
 })
