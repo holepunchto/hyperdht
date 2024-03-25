a0c3d91334ffb16bcc8869dbceec7cd392a15640
Tobias Baunbaek <freeall@gmail.com>
2024-03-25 14:08:01 +0100

    Remove keet-hypertrace-logger

--- test/integration/fixtures/server-through-relay.js
+++ test/integration/fixtures/server-through-relay.js
@@ -1,19 +1,19 @@
 const DHT = require('../../../')
-const { Client: KHLClient } = require('keet-hypertrace-logger')
+// const { Client: KHLClient } = require('keet-hypertrace-logger')
 
 const publicKey = Buffer.from(process.argv[2], 'hex')
 const secretKey = Buffer.from(process.argv[3], 'hex')
 const keyPair = { publicKey, secretKey }
-const khlClient = new KHLClient()
-khlClient.start({
-  createSocket: () => {
-    const node = new DHT()
-    return node.connect(Buffer.from('17ae5b10a5abdc269e16d740c1eb762f215c05a697c7e37c996abfcc488e82f3', 'hex'), {
-      keyPair
-    })
-  },
-  getInitialProps: () => ({ alias: 'server' })
-})
+// const khlClient = new KHLClient()
+// khlClient.start({
+//   createSocket: () => {
+//     const node = new DHT()
+//     return node.connect(Buffer.from('17ae5b10a5abdc269e16d740c1eb762f215c05a697c7e37c996abfcc488e82f3', 'hex'), {
+//       keyPair
+//     })
+//   },
+//   getInitialProps: () => ({ alias: 'server' })
+// })
 
 main()
 
@@ -23,7 +23,7 @@ async function main () {
     holepunch: false, // To ensure it relies only on relaying
     shareLocalAddress: false, // To help ensure it relies only on relaying (otherwise it can connect directly over LAN, without even trying to holepunch)
     relayThrough: [
-      // Buffer.from('45ae429318f146326dddb27168532c7c6b21cacfdd4a43d539e06bd518a7893a', 'hex'),
+      Buffer.from('45ae429318f146326dddb27168532c7c6b21cacfdd4a43d539e06bd518a7893a', 'hex'),
       Buffer.from('26eb24c97e53f94d392842b3c0b3fddcb903a0883ac5691e67e4c9d369ef2332', 'hex'),
       Buffer.from('5c4ee2d0140670b433c0f844fe38264c022842cd9b76b5d28767b462531dfeb2', 'hex'),
       Buffer.from('8e2a691a6e0b0ede66bd45752b0165514fbec8844721eb038fbcc412af0eb691', 'hex'),
