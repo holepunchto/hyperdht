5b847591380e34f3e55d5d32ffad14b42af6974a
Tobias Baunbaek <freeall@gmail.com>
2024-03-25 11:45:13 +0100

    Re-add localConnection: false

--- examples/connection-relaying/client.js
+++ examples/connection-relaying/client.js
@@ -4,6 +4,7 @@ const [relay, server] = process.argv.slice(2)
 
 const dht = new DHT()
 const socket = dht.connect(Buffer.from(server, 'hex'), {
+  localConnection: false,
   relayThrough: Buffer.from(relay, 'hex')
 })
 
