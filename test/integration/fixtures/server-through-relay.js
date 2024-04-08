c0c9ee988b230c1a151fe84fa57f8ad3c017205e
Tobias Baunbaek <freeall@gmail.com>
2024-04-08 16:02:54 +0200

    Add relay test

--- test/integration/fixtures/server-through-relay.js
+++ test/integration/fixtures/server-through-relay.js
@@ -1,20 +1,11 @@
 const DHT = require('../../../')
-const hic = require('hypercore-id-encoding')
-// const { Client: KHLClient } = require('keet-hypertrace-logger')
 
 const publicKey = Buffer.from(process.argv[2], 'hex')
 const secretKey = Buffer.from(process.argv[3], 'hex')
+const relayServer = Buffer.from(process.argv[4], 'hex')
+const socketKeepAlive = Number(process.argv[5] || 5000)
+const relayKeepAlive = Number(process.argv[6] || 5000)
 const keyPair = { publicKey, secretKey }
-// const khlClient = new KHLClient()
-// khlClient.start({
-//   createSocket: () => {
-//     const node = new DHT()
-//     return node.connect(Buffer.from('17ae5b10a5abdc269e16d740c1eb762f215c05a697c7e37c996abfcc488e82f3', 'hex'), {
-//       keyPair
-//     })
-//   },
-//   getInitialProps: () => ({ alias: 'server' })
-// })
 
 main()
 
@@ -23,22 +14,10 @@ async function main () {
   const server = node.createServer({
     holepunch: false, // To ensure it relies only on relaying
     shareLocalAddress: false, // To help ensure it relies only on relaying (otherwise it can connect directly over LAN, without even trying to holepunch)
-    // relayKeepAlive: 100,
-    // relayThrough: [
-    //   // hic.decode('8684i4hjjcjqxh6or7kxnidwzqej8z8mi91tuw54k6j548gto7ky')
-    //   hic.decode('okyufaztgzc3143c3hmxt35bj63bitnrxtszn4bi36ykn3a1qbqy')
-    // ]
-    relayThrough: [
-      Buffer.from('45ae429318f146326dddb27168532c7c6b21cacfdd4a43d539e06bd518a7893a', 'hex'),
-      Buffer.from('26eb24c97e53f94d392842b3c0b3fddcb903a0883ac5691e67e4c9d369ef2332', 'hex'),
-      Buffer.from('5c4ee2d0140670b433c0f844fe38264c022842cd9b76b5d28767b462531dfeb2', 'hex'),
-      Buffer.from('8e2a691a6e0b0ede66bd45752b0165514fbec8844721eb038fbcc412af0eb691', 'hex'),
-      Buffer.from('74bd888061f419745bd011367710e0ba98e0db0a2fb12ae1a21ba2d13d75a30c', 'hex'),
-      Buffer.from('1dffaffb7cfe080b15aefae5fa18c2b7ad43facc8882b5d614fd45262f33e9c9', 'hex'),
-      Buffer.from('f1154be6dcc4f98f38ab4dbfe751457907b14dac3c76d1ed654aa65c690c2968', 'hex')
-    ]
+    relayKeepAlive,
+    relayThrough: relayServer
   }, socket => {
-    socket.setKeepAlive(5000)
+    socket.setKeepAlive(socketKeepAlive)
     socket
       .on('data', data => {
         console.log(`socket_ondata ${data.toString()}`)
---
+++ test/integration/keep-alive-with-relay.js
@@ -0,0 +1,140 @@
+const test = require('brittle')
+const { spawnFixture } = require('../helpers')
+const { Server: RelayServer } = require('blind-relay')
+const DHT = require('../../')
+const path = require('path')
+
+/*
+  When a peer connects to a relay server, there's two sockets established. One for data (between two peers)
+  and another "control" socket.
+  The relay server has a RELAY_KEEPALIVE that looks at the control socket to detect when a peer is no longer there.
+  The client and server both have a SOCKET_KEEPALIVE which detects when the other peer is no longer there.
+
+  A bug that occured with udx-native < 1.8.9 is that the relay server had detected the control socket had bad been destroyed,
+  but didn't fully clean up internally. That meant that this information was never fully relayed to the other peer.
+*/
+test('When Server is killed, Client should detect this - through relay', async t => {
+  t.plan(3)
+
+  const relayTest = t.test('relay')
+  relayTest.plan(3)
+  const clientTest = t.test('client')
+  clientTest.plan(3)
+  const serverTest = t.test('server')
+  serverTest.plan(3)
+
+  const RELAY_KEEPALIVE = 500
+  const SOCKET_KEEPALIVE = 10 * RELAY_KEEPALIVE
+
+  const clientKeyPair = DHT.keyPair()
+  const clientNode = new DHT()
+  const relayNode = new DHT()
+  const relayKeyPair = DHT.keyPair()
+  const serverKeyPair = DHT.keyPair()
+  const serverPublicKey = serverKeyPair.publicKey.toString('hex')
+  const serverSecretKey = serverKeyPair.secretKey.toString('hex')
+  let hasClientDetectedThatServerDied = false
+  let didClientNotDetectThatServerDiedTimer
+  let didClientNotDetectThatServerDiedTimerFired = false
+
+  t.teardown(async () => {
+    await clientNode.destroy()
+    await relayNode.destroy()
+  })
+
+  await startRelayServer()
+  startServer()
+
+  async function startRelayServer () {
+    const relay = new RelayServer({
+      createStream (opts) {
+        return relayNode.createRawStream({ ...opts, framed: true })
+      }
+    })
+
+    const relayServer = relayNode.createServer(socket => {
+      relayTest.pass('Socket connected')
+      socket.setKeepAlive(RELAY_KEEPALIVE)
+
+      socket.on('error', err => {
+        // when error is ETIMEDOUT it's the server connection that has broken
+        // not so long after that, the client should have detected that the connection is gone
+        if (err.code === 'ETIMEDOUT') {
+          relayTest.pass('Relay server detected that server has died. Waiting for client to detect')
+
+          // In some cases, the client may have detected that the server has died before the relay server
+          if (hasClientDetectedThatServerDied) return
+
+          const timeToDetectClientHasDied = SOCKET_KEEPALIVE
+          didClientNotDetectThatServerDiedTimer = setTimeout(() => {
+            didClientNotDetectThatServerDiedTimerFired = true
+            clientTest.fail('Client did not detect that the server has died')
+          }, timeToDetectClientHasDied)
+        }
+      })
+
+      const session = relay.accept(socket, { id: socket.remotePublicKey })
+      session.on('error', () => { })
+    })
+
+    await relayServer.listen(relayKeyPair)
+  }
+
+  async function startServer () {
+    const args = [
+      path.join(__dirname, 'fixtures/server-through-relay.js'),
+      serverPublicKey,
+      serverSecretKey,
+      relayKeyPair.publicKey.toString('hex'),
+      SOCKET_KEEPALIVE,
+      RELAY_KEEPALIVE
+    ]
+
+    for await (const [kill, data] of spawnFixture(serverTest, args)) {
+      if (data === 'started') {
+        serverTest.pass('Started. Now starting new client')
+        startClient()
+      }
+      if (data === 'socket_ondata hello') {
+        serverTest.pass('Received "hello" from client. Sending "world" back, then wait 1 second and kill server')
+        setTimeout(kill, 1000)
+      }
+    }
+
+    serverTest.pass('Server process killed. Waiting for relay server to detect')
+  }
+
+  function startClient () {
+    const client = clientNode.connect(serverKeyPair.publicKey, {
+      keyPair: clientKeyPair, // To ensure same client keyPair on each connection
+      relayKeepAlive: RELAY_KEEPALIVE,
+      relayThrough: relayKeyPair.publicKey
+    })
+    client.setKeepAlive(SOCKET_KEEPALIVE)
+    // If the client does not receive a ETIMEDOUT it's because it has not detected
+    // that the server has been killed. Important to note that all of this is through
+    // the relay server. Essentially it's whether or not the relay server
+    // detected that the server had been killed and relayed that information downstream
+    // to the client
+    client
+      .on('error', () => { })
+      .on('open', () => {
+        clientTest.pass('Socket opened. Now sending "hello"')
+        client.write('hello')
+      })
+      .on('data', data => {
+        data = data.toString()
+
+        if (data === 'world') {
+          clientTest.pass('Received "world" from server')
+        }
+      })
+      .on('close', () => {
+        if (didClientNotDetectThatServerDiedTimerFired) return // If this has fired, then it's too late
+
+        hasClientDetectedThatServerDied = true
+        clearTimeout(didClientNotDetectThatServerDiedTimer)
+        clientTest.pass('Client correctly detected that server had died')
+      })
+  }
+})
