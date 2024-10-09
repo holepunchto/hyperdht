1e801bec284ed0659956dafa545a483e11e63a8c
Mathias Buus <mathiasbuus@gmail.com>
2024-10-09 20:52:10 +0200

    remove debug option as its unused now (#204)

--- index.js
+++ index.js
@@ -46,9 +46,6 @@ class HyperDHT extends DHT {
     this._randomPunches = 0
     this._randomPunchLimit = 1 // set to one for extra safety for now
 
-    this._debugStream = (opts.debug && opts.debug.stream) || null
-    this._debugHandshakeLatency = toRange((opts.debug && opts.debug.handshake && opts.debug.handshake.latency) || 0)
-
     this.once('persistent', () => {
       this._persistent = new Persistent(this, persistent)
     })
@@ -534,11 +531,6 @@ function mapMutable (node) {
 }
 
 function noop () {}
-
-function toRange (n) {
-  if (!n) return null
-  return typeof n === 'number' ? [n, n] : n
-}
 
 function filterNode (node) {
   // always skip these testnet nodes that got mixed in by accident, until they get updated
