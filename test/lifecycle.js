91c6386fa1be0d3131b202340131e39ad0baa359
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-11-22 09:40:08 +0100

    Throw error when reusing keyPair for listens (#208)
    * Throw error when reusing keyPair for listens
    
    * Do not delete server from listening if it throws KEYPAIR_ALREADY_USED
    
    * Do not remove server from listening set if announcer.start() throws
    
    * Always clean up if _listen throws before starting the announcer

--- test/lifecycle.js
+++ test/lifecycle.js
@@ -1,6 +1,7 @@
 const test = require('brittle')
-const { swarm } = require('./helpers')
 const safetyCatch = require('safety-catch')
+const hypCrypto = require('hypercore-crypto')
+const { swarm } = require('./helpers')
 
 test('Can destroy a DHT node while server.listen() is called', async function (t) {
   const [a] = await swarm(t)
@@ -15,4 +16,30 @@ test('Can destroy a DHT node while server.listen() is called', async function (t
 
   await listenProm
   t.pass('The listen function does not error when the DHT closes while it is running')
+})
+
+test('Cannot listen on multiple servers with the same keypair', async function (t) {
+  const [a] = await swarm(t)
+
+  const s1 = a.createServer()
+  const s2 = a.createServer()
+
+  const s3 = a.createServer()
+  const s4 = a.createServer()
+  const s5 = a.createServer()
+
+  await s1.listen()
+  await t.exception(
+    async () => await s2.listen(),
+    /KEYPAIR_ALREADY_USED/
+  )
+
+  const keyPair = hypCrypto.keyPair()
+
+  await s3.listen(keyPair)
+  await t.exception(
+    async () => await s4.listen(keyPair),
+    /KEYPAIR_ALREADY_USED/
+  )
+  await s5.listen(hypCrypto.keyPair())
 })
