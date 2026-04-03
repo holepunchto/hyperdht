c3f378a13f933ef2347dfbaa8695a03b9e4cd4df
Noah Levenson <noahlevenson@gmail.com>
2026-04-03 09:50:26 -0700

    rm control plane document record API

--- index.js
+++ index.js
@@ -449,44 +449,6 @@ class HyperDHT extends DHT {
     return { target: target, indexID: indexID, closestNodes: query.closestNodes }
   }
 
-  async phtShardGet(target, key, opts = {}) {
-    opts = { ...opts, map: mapPHTShard }
-    const query = this.query({ target, command: COMMANDS.PHT_SHARD_GET, value: key }, opts)
-
-    for await (const node of query) {
-      const { value } = node
-      
-      // TODO: merge the responses
-
-      return node
-    }
-
-    return null
-  }
-
-  async phtShardPut(target, key, value, opts = {}) {
-    const encoded = c.encode(m.phtShardPutRequest, {
-      key: key,
-      value: value
-    })
-
-    opts = {
-      ...opts,
-      map: mapPHTShard,
-      commit(reply, dht) {
-        return dht.request(
-          { token: reply.token, target, command: COMMANDS.PHT_SHARD_PUT, value: encoded },
-          reply.from
-        )
-      }
-    }
-
-    const query = this.query({ target, command: COMMANDS.PHT_SHARD_GET, value: key }, opts)
-    await query.finished()
-
-    return { closestNodes: query.closestNodes }
-  }
-
   onrequest(req) {
     switch (req.command) {
       case COMMANDS.PEER_HANDSHAKE: {
@@ -540,14 +502,6 @@ class HyperDHT extends DHT {
       }
       case COMMANDS.AUTHENTICATED_PHT_NODE_GET: {
         this._persistent.onauthenticatedphtnodeget(req)
-        return true
-      }
-      case COMMANDS.PHT_SHARD_PUT: {
-        this._persistent.onphtshardput(req)
-        return true
-      }
-      case COMMANDS.PHT_SHARD_GET: {
-        this._persistent.onphtshardget(req)
         return true
       }
     }
